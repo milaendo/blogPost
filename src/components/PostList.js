2389469ba82122f9dd67727c30a000cf015ddc90
Mila Endo <milal.alim@gmail.com>
2017-09-07 14:39:16 -0700

    display posts

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,10 +1,37 @@
 import React, { Component } from 'react'
+import {Link} from 'react-router-dom'
 
 class PostList extends Component {
+	state = {
+		
+		posts:[]
+		
+	}
+	componentWillMount(){
+		fetch('https://tiny-lasagna-server.herokuapp.com/collections/blogger/')
+		.then(response => 
+			{return response.json()})
+		.then(data =>{
+		    console.log (data)
+		    this.setState({
+		      posts: data
+		    })
+		  })
+	}
 	render () {
 		return (
-			<div>
-			<h1>PostList</h1>
+			<div className='postWrap'>
+			<h1>Blog Titles</h1>
+			{this.state.posts.map(function(post){
+				return (
+				<div key={post._id}>
+				<Link to = {'/ShowPost/' + post._id}>
+					<p>{post.title}</p>
+				</Link>
+				</div>
+
+			)})}
+		
 			</div>
 		)
 	}
--- src/components/ShowPost.js
+++ src/components/ShowPost.js
@@ -1,12 +1,36 @@
 import React, { Component } from 'react'
 
 class ShowPost extends Component {
-
+	state={
+			title:'',
+			name:'',
+			blog:''
+		}
+	componentWillMount(){
+		const id = this.props.match.params.id 
+		fetch('https://tiny-lasagna-server.herokuapp.com/collections/blogger/' + id)
+		.then(response => {
+			return response.json()})
+		.then(resp => {
+			console.log(resp)
+			this.setState({
+				title:resp.title,
+				name:resp.name,
+				blog:resp.blog
+			})
+		})
+	}
+ 
 	render () {
 		return (
 			<div>
 			<h1>ShowPost</h1>
-			{this.props.match.params.id}
+			<h3>Title:</h3>
+			<p>{this.state.title}</p>
+			<h3>Author:</h3>
+			<p>{this.state.name}</p>
+			<h3>Post:</h3>
+			<p>{this.state.blog}</p>
 			</div>
 		)
 	}
